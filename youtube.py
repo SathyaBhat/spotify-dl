11a0e7c198254ef2b2bbab873ba2bc07c8ab4949
Roberto Romero <sildurin@gmail.com>
2016-05-19 16:21:45 +0200

    Fix error: UnicodeEncodeError: 'ascii' codec can't encode character u'\xe9' in position 5: ordinal not in range(128) (#15)

--- youtube.py
+++ youtube.py
@@ -5,7 +5,7 @@ from scaffold import log
 
 def fetch_youtube_url(search_term):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=YOUTUBE_DEV_KEY)
-    log.info("Searching for {}".format(search_term))
+    log.info(u"Searching for {}".format(search_term))
     search_response = youtube.search().list(q=search_term, part='id, snippet').execute()
     for v in search_response['items']:
         if v['id']['kind'] == VIDEO:
