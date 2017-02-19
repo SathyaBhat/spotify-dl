182dfd6801d7b89c17caf2f27473164d7e3aba86
SathyaBhat <sathya@sathyasays.com>
2017-02-20 00:31:08 +0530

    bump up version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except (IOError, ImportError):
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-version = '2.1.0'
+version = '2.1.1'
 
 setup(
     name='spotify_dl',
