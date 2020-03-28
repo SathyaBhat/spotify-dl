b6939126efbc1b50819dbb16bf295ebed98f7725
Sathyajith Bhat <sathya@sathyasays.com>
2020-03-29 00:07:46 +0200

    move sentry around

--- spotify_dl/__init__.py
+++ spotify_dl/__init__.py
@@ -1,6 +1,5 @@
 import signal
 import sys
-import sentry_sdk
 
 def signal_handler(sig, frame):
     # Signal handler to handle SIGINT, usually when Ctrl+C is pressed.. or if SIGINT is sent.
@@ -8,5 +7,3 @@ def signal_handler(sig, frame):
     print('\nCaught interrupt(did you press Ctrl+C?), stopping spotify_dl')
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
-sentry_sdk.init("https://7d74a39472c9449dac51eb24bb33bdc3@sentry.io/2383261")
