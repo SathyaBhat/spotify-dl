ad637625a300b6b65a82466616e5edb666427642
e3oroush <ebrahim.soroush@gmail.com>
2020-04-11 02:50:14 +0430

    changes test file to pass the test

--- tests/test_youtube_url.py
+++ tests/test_youtube_url.py
@@ -1,12 +1,10 @@
-from spotify_dl.youtube import fetch_youtube_url, get_youtube_dev_key
+from spotify_dl.youtube import fetch_youtube_url
 
 
 def test_fetch_youtube_url(capsys):
-    song_link = fetch_youtube_url("Red Hot Chili Peppers - Dani California [Official Music Video]",
-                                    "12354")
-    assert song_link is None
-    captured = capsys.readouterr()
-    assert "keyInvalid" in captured.out
-    song_link = fetch_youtube_url("Red Hot Chili Peppers - Dani California [Official Music Video]",
-                                    get_youtube_dev_key())
+    # song_link = fetch_youtube_url("Red Hot Chili Peppers - Dani California [Official Music Video]")
+    # assert song_link is None
+    # captured = capsys.readouterr()
+    # assert "keyInvalid" in captured.out
+    song_link = fetch_youtube_url("Red Hot Chili Peppers - Dani California [Official Music Video]")
     assert song_link == 'https://www.youtube.com/watch?v=Sb5aq5HcS1A'
