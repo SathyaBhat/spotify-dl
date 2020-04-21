57f3758294d3037cd38479bd3d0cd47ba38eef2d
Luca Koroll <contact@lucakoroll.de>
2020-04-21 20:39:44 +0200

    Add HTML Scraping functionality

--- spotify_dl/scaffold.py
+++ spotify_dl/scaffold.py
@@ -12,7 +12,7 @@ log = logging.getLogger('sdl')
 sentry_sdk.init("https://7d74a39472c9449dac51eb24bb33bdc3@sentry.io/2383261")
 
 
-def check_for_tokens():
+def check_for_tokens(args=None):
     log.debug('Checking for tokens')
     CLIENT_ID = getenv('SPOTIPY_CLIENT_ID')
     CLIENT_SECRET = getenv('SPOTIPY_CLIENT_SECRET')
@@ -45,6 +45,10 @@ def check_for_tokens():
 
             Generate the key from
             https://console.developers.google.com/apis/api/youtube/overview
+            
+            Or use the HTML Scraper by specifying -s True
             ''')
+        if args.scrape:
+            return True
         return False
     return True
--- spotify_dl/youtube.py
+++ spotify_dl/youtube.py
@@ -10,39 +10,54 @@ from spotify_dl.constants import VIDEO
 from spotify_dl.constants import YOUTUBE_VIDEO_URL
 from spotify_dl.scaffold import log
 from json import loads
+import requests
+from lxml import html
+import re
 
 from click import secho
 
-def fetch_youtube_url(search_term, dev_key):
+def fetch_youtube_url(search_term, dev_key=None, scrape=False):
     """For each song name/artist name combo, fetch the YouTube URL
         and return the list of URLs"""
-    YOUTUBE_DEV_KEY = dev_key
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-                    developerKey=YOUTUBE_DEV_KEY,
-                    cache_discovery=False)
     log.info(u"Searching for {}".format(search_term))
-    try:
-        search_response = youtube.search().list(q=search_term,
-                                                part='id, snippet').execute()
-        for v in search_response['items']:
-            if v['id']['kind'] == VIDEO:
-                log.debug("Adding Video id {}".format(v['id']['videoId']))
-                return YOUTUBE_VIDEO_URL + v['id']['videoId']
-    except HttpError as err: 
-        err_details = loads(err.content.decode('utf-8')).get('error').get('errors')
-        secho("Couldn't complete search due to following errors: ", fg='red')
-        for e in err_details:
-            error_reason = e.get('reason')
-            error_domain = e.get('domain')
-            error_message = e.get('message')
+    if scrape or not dev_key:
+        YOUTUBE_SEARCH_BASE = "https://www.youtube.com/results?search_query="
+        try:
+            response = html.fromstring(requests.get(YOUTUBE_SEARCH_BASE + search_term).content)
+            video = response.xpath("//a[contains(@class, 'yt-uix-tile-link')]/@href")
+            video_id = re.search("((\?v=)[a-zA-Z0-9_-]{4,15})", video[0]).group(0)[3:]
+            print(video_id)
+            return YOUTUBE_VIDEO_URL + video_id
+        except:
+            print("Could not perform scrape search")
+            return None
+    else:
+        YOUTUBE_DEV_KEY = dev_key
+        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                        developerKey=YOUTUBE_DEV_KEY,
+                        cache_discovery=False)
+        try:
+            search_response = youtube.search().list(q=search_term,
+                                                    part='id, snippet').execute()
+            for v in search_response['items']:
+                if v['id']['kind'] == VIDEO:
+                    log.debug("Adding Video id {}".format(v['id']['videoId']))
+                    return YOUTUBE_VIDEO_URL + v['id']['videoId']
+        except HttpError as err:
+            err_details = loads(err.content.decode('utf-8')).get('error').get('errors')
+            secho("Couldn't complete search due to following errors: ", fg='red')
+            for e in err_details:
+                error_reason = e.get('reason')
+                error_domain = e.get('domain')
+                error_message = e.get('message')
 
-            if error_reason == 'quotaExceeded' or error_reason == 'dailyLimitExceeded':
-                secho(f"\tYou're over daily allowed quota. Unfortunately, YouTube restricts API keys to a max of 10,000 requests per day which translates to a maximum of 100 searches.", fg='red')
-                secho(f"\tThe quota will be reset at midnight Pacific Time (PT)." ,fg='red')
-                secho(f"\tYou can request for Quota increase from https://console.developers.google.com/apis/api/youtube.googleapis.com/quotas.", fg='red')
-            else:
-                secho(f"\t Search failed due to {error_domain}:{error_reason}, message: {error_message}")
-        return None
+                if error_reason == 'quotaExceeded' or error_reason == 'dailyLimitExceeded':
+                    secho(f"\tYou're over daily allowed quota. Unfortunately, YouTube restricts API keys to a max of 10,000 requests per day which translates to a maximum of 100 searches.", fg='red')
+                    secho(f"\tThe quota will be reset at midnight Pacific Time (PT)." ,fg='red')
+                    secho(f"\tYou can request for Quota increase from https://console.developers.google.com/apis/api/youtube.googleapis.com/quotas.", fg='red')
+                else:
+                    secho(f"\t Search failed due to {error_domain}:{error_reason}, message: {error_message}")
+            return None
     
 def get_youtube_dev_key():
     return getenv('YOUTUBE_DEV_KEY')
