642196eb91b58f9f034a03421fbc149e62f066e5
Sathyajith <sabhat@adobe.com>
2020-05-02 21:53:31 +0300

    some additional error handling when scraping fails

--- spotify_dl/youtube.py
+++ spotify_dl/youtube.py
@@ -2,6 +2,7 @@ from os import getenv
 
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpError
+from sentry_sdk import capture_exception
 
 
 from spotify_dl.constants import YOUTUBE_API_SERVICE_NAME
@@ -14,22 +15,29 @@ import requests
 from lxml import html
 import re
 
+
 from click import secho
 
 def fetch_youtube_url(search_term, dev_key=None, scrape=False):
     """For each song name/artist name combo, fetch the YouTube URL
         and return the list of URLs"""
-    log.info(u"Searching for {}".format(search_term))
+    log.info(f"Searching for {search_term}")
     if scrape or not dev_key:
         YOUTUBE_SEARCH_BASE = "https://www.youtube.com/results?search_query="
         try:
-            response = html.fromstring(requests.get(YOUTUBE_SEARCH_BASE + search_term).content)
-            video = response.xpath("//a[contains(@class, 'yt-uix-tile-link')]/@href")
+            response = requests.get(YOUTUBE_SEARCH_BASE + search_term).content
+            html_response = html.fromstring(response)
+            video = html_response.xpath("//a[contains(@class, 'yt-uix-tile-link')]/@href")
             video_id = re.search("((\?v=)[a-zA-Z0-9_-]{4,15})", video[0]).group(0)[3:]
-            print(video_id)
+            log.debug(f"Found video id {video_id} for search term {search_term}")
             return YOUTUBE_VIDEO_URL + video_id
-        except:
-            print("Could not perform scrape search")
+        except AttributeError as e:
+            log.warn(f"Could not find scrape details for {search_term}")
+            capture_exception(e)
+            return None
+        except IndexError as e:
+            log.warn(f"Could not perform scrape search for {search_term}, diff HTML")
+            capture_exception(e)
             return None
     else:
         YOUTUBE_DEV_KEY = dev_key
