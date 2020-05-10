74794b84edb9e79c1b1a7533ff14c8255487b721
Sathyajith <sathya@sathyasays.com>
2020-05-10 09:56:15 +0300

    remove validate.py, add later

--- spotify_dl/validate.py
+++
@@ -1,8 +0,0 @@
-
-def validate_spotify_playlist_url(playlist_url):
-    url_match = playlist_url_pattern.match(args.url)
-    if args.url and url_match and len(url_match.groups()) > 0:
-        uri = "spotify:" + url_match.groups()[0].replace('/', ':')
-        args.uri = [uri]
-    else:
-        raise Exception('Invalid playlist URL ')
