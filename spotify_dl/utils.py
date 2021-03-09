0e6b7e1eec89f1135891976c84f0af8767bea53c
Jorge Daniel Sampayo Vargas <jdsampayo@gmail.com>
2021-03-09 15:32:15 -0600

    Add dollar sign to clean up list

--- spotify_dl/utils.py
+++ spotify_dl/utils.py
@@ -4,7 +4,7 @@ def sanitize(name, replace_with=''):
     :param name: Name to be cleaned up
     :return string containing the cleaned name
     """
-    clean_up_list = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0"]
+    clean_up_list = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0", "$"]
     for x in clean_up_list:
         name = name.replace(x, replace_with)
     return name
