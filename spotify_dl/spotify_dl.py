61c1a1f575b2324995760b225a57f65294bd732d
kithstack1 <106423962+kithstack1@users.noreply.github.com>
2022-09-27 11:13:33 +0300

    Update spotify_dl.py
    validated number of cpu cores requested

--- spotify_dl/spotify_dl.py
+++ spotify_dl/spotify_dl.py
@@ -104,7 +104,11 @@ def spotify_dl():
         help="Use multiprocessing [-m [int:numcores]",
     )
     args = parser.parse_args()
+    num_cores = os.cpu_count()
     args.multi_core = int(args.multi_core)
+    if args.multi_core > (num_cores - 1):
+        print('[!] too many cores requested , reverting to {numcores - 1} cores')
+        args.multi_core = numcores - 1
     if args.version:
         console.print(f"spotify_dl [bold green]v{VERSION}[/bold green]")
         sys.exit(0)
