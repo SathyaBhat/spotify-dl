80b92c704b971059178ff44ee2f855f1df1455f6
krishsharma0413 <krishsharma0413@gmail.com>
2023-12-30 17:28:44 +0530

    Add tempo information to track metadata

--- spotify_dl/youtube.py
+++ spotify_dl/youtube.py
@@ -71,6 +71,7 @@ def write_tracks(tracks_file, song_dict):
                 track_artist = track["artist"]
                 track_num = track["num"]
                 track_album = track["album"]
+                track_tempo = track["tempo"]
                 track["save_path"] = url_dict["save_path"]
                 track_db.append(track)
                 track_index = i
@@ -81,6 +82,7 @@ def write_tracks(tracks_file, song_dict):
                     track_url,
                     str(track_num),
                     track_album,
+                    str(track_tempo),
                     str(track_index),
                 ]
                 try:
@@ -119,6 +121,7 @@ def set_tags(temp, filename, kwargs):
         )
 
     song_file["genre"] = song.get("genre")
+    song_file["bpm"] = str(song.get("tempo"))
     song_file.save()
     song_file = MP3(filename, ID3=ID3)
     cover = song.get("cover")
@@ -150,10 +153,11 @@ def find_and_download_songs(kwargs):
     with open(reference_file, "r", encoding="utf-8") as file:
         for line in file:
             temp = line.split(";")
-            name, artist, album, i = (
+            name, artist, album, tempo, i = (
                 temp[0],
                 temp[1],
                 temp[4],
+                temp[5],
                 int(temp[-1].replace("\n", "")),
             )
 
