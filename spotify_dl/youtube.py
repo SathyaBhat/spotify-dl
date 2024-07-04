934b6eef768e57f4e5de7b8600fdb24889199923
jan Mikowa <66056347+Maritsu@users.noreply.github.com>
2024-07-05 01:27:14 +0200

    Various search improvements (#375)
    * remove "Lyrics" keyword from search
    
    * search with Youtube Music for better results (#373,#374)
    
    - add ytmusicapi as dependency
    - modify dump_json() and find_and_download_songs() to use YT Music
    
    * add function to get closest matching string via Levenshtein edit distance
    
    * implement getting closest match in search results
    
    * update CI and package setup to python>=3.8
    
    * Update .github/workflows/tests.yml
    
    ---------
    
    Co-authored-by: Sathyajith Bhat <sathya@sathyasays.com>

--- spotify_dl/youtube.py
+++ spotify_dl/youtube.py
@@ -7,11 +7,12 @@ import json
 import mutagen
 import csv
 import yt_dlp
+import ytmusicapi
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import APIC, ID3
 from mutagen.mp3 import MP3
 from spotify_dl.scaffold import log
-from spotify_dl.utils import sanitize
+from spotify_dl.utils import sanitize, get_closest_match
 from spotify_dl.constants import DOWNLOAD_LIST
 
 
@@ -33,7 +34,7 @@ def dump_json(songs):
     :param songs: the songs for which the JSON should be output
     """
     for song in songs:
-        query = f"{song.get('artist')} - {song.get('name')} Lyrics".replace(
+        query = f"{song.get('artist')} - {song.get('name')}".replace(
             ":", ""
         ).replace('"', "")
 
@@ -41,8 +42,18 @@ def dump_json(songs):
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             try:
-                ytJson = ydl.extract_info("ytsearch:" + query, False)
-                print(json.dumps(ytJson.get("entries")))
+                ytJson = {}
+                with ytmusicapi.YTMusic() as ym:
+                    # Reduce results to array of titles and video IDs
+                    result_titles, result_ids = zip(*map(
+                        lambda d: (f"{d['artists'][0]['name']} - {d['title']}".replace(":", "").replace('"', ""), d["videoId"]),
+                        ym.search(query, filter="songs")
+                    ))
+                    # Get ID of closest matching result by finding index in titles list
+                    videoId = result_ids[result_titles.index(get_closest_match(result_titles, query))]
+
+                    ytJson = ydl.extract_info(f"https://music.youtube.com/watch?v={videoId}", False)
+                print(json.dumps([ytJson]))  # insert into array so that the format stays the same
             except Exception as e:  # skipcq: PYL-W0703
                 log.debug(e)
                 print(
@@ -144,7 +155,7 @@ def set_tags(temp, filename, kwargs):
 def find_and_download_songs(kwargs):
     """
     function handles actual download of the songs
-    the youtube_search lib is used to search for songs and get best url
+    the ytmusicapi lib is used to search for songs and get best url via YT Music
     :param kwargs: dictionary of key value arguments to be used in download
     """
     sponsorblock_postprocessor = []
@@ -160,7 +171,7 @@ def find_and_download_songs(kwargs):
                 int(temp[-1].replace("\n", "")),
             )
 
-            query = f"{artist} - {name} Lyrics".replace(":", "").replace('"', "")
+            query = f"{artist} - {name}".replace(":", "").replace('"', "")
             print(f"Initiating download for {query}.")
 
             file_name = kwargs["file_name_f"](
@@ -200,6 +211,15 @@ def find_and_download_songs(kwargs):
                 print(f"File {mp3file_path} already exists, we do not overwrite it ")
                 continue
 
+            with ytmusicapi.YTMusic() as ym:
+                # Reduce search results to array of titles and video IDs
+                result_titles, result_ids = zip(*map(
+                    lambda d: (f"{d['artists'][0]['name']} - {d['title']}".replace(":", "").replace('"', ""), d["videoId"]),
+                    ym.search(query, filter="songs")
+                ))
+                # Get ID of closest matching result by finding index in titles list
+                video_id = result_ids[result_titles.index(get_closest_match(result_titles, query))]
+
             outtmpl = f"{file_path}.%(ext)s"
             ydl_opts = {
                 "proxy": kwargs.get("proxy"),
@@ -227,7 +247,7 @@ def find_and_download_songs(kwargs):
                 ydl_opts["postprocessors"].append(mp3_postprocess_opts.copy())
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 try:
-                    ydl.download([query])
+                    ydl.download([f"https://music.youtube.com/watch?v={video_id}"])
                 except Exception as e:  # skipcq: PYL-W0703
                     log.debug(e)
                     print(f"Failed to download {name}, make sure yt_dlp is up to date")
